fcce47858396a80b0dbe7c3873fc26f9facf5779
mayu.ito <shigmastudy@gmail.com>
2021-03-13 09:02:43 +0000

    user registration

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -9,12 +9,12 @@
         
         <div class="collapse navbar-collapse" id="nav-bar">
             <ul class="navbar-nav mr-auto"></ul>
-            <ul class="navbar-nav">
-                {{--ユーザー登録ページへのリンク--}}
-                <li class="nav-item"><a href="#" class="nav-link">Signup</a></li>
-                {{--ログインページへのリンク--}}
-                <li class="nav-item"><a href="#" class="nav-link">Login</a></li>
-            </ul>
+             <ul class="nav navbar-nav navbar-right">
+                    {{-- ユーザ登録ページへのリンク --}}
+                    <li>{!! link_to_route('signup.get', 'Signup', [], ['class' => 'nav-link']) !!}</li>
+                    {{-- ログインページへのリンク --}}
+                    <li><a href="#">Login</a></li>
+                </ul>
         </div>
     </nav>
 </header>
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,10 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// ユーザ登録
+Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
+Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
