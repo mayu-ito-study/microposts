86409459574b98443ad51ae759865c35e425e125
mayu.ito <shigmastudy@gmail.com>
2021-03-15 16:26:00 +0000

    user pages

---
+++ resources/views/users/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+    {{--ユーザー一覧--}}
+    @include('users.users')
+@endsection
+    
--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,6 @@ Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
 Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login')->name('login.post');
 Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
+Route::group(['middleware' => ['auth']], function() {
+    Route::resource('users', 'UsersController', ['only' => ['index', 'show']]);
+});
